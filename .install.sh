04a396bf7020e9c7ba0cdbd59f325e0e3a8192fc
Rainer Hind <1928782+rai-hi@users.noreply.github.com>
2019-09-15 20:34:03 +0100

    Add FZF to install

---
+++ .install.sh
@@ -0,0 +1,9 @@
+brew install python
+brew install python3
+pip2 install neovim --upgrade
+pip3 install neovim --upgrade
+brew install neovim
+brew install fzf
+
+# To install useful key bindings and fuzzy completion:
+$(brew --prefix)/opt/fzf/install
